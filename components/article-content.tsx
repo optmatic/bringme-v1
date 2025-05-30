c9748fc4c1e3538cc3fa0683beeb5c578e9ed227
thomas <tom@optmatic.com>
2025-05-30 18:17:53 +1000

    prose fixes

--- components/article-content.tsx
+++ components/article-content.tsx
@@ -190,7 +190,7 @@ export function ArticleContent({ article }: ArticleContentProps) {
             </div>
 
             {/* Title, Excerpt & Image Container */}
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 p-12 lg:p-16">
+            <div className="flora-fauna-bg grid grid-cols-1 lg:grid-cols-3 gap-8 p-6 lg:p-8 rounded-xl shadow-sm">
               <div className="lg:col-span-2 space-y-6">
                 <h1
                   className={
